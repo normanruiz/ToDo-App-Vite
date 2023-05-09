ba9112b06690eef660f62e6fc3c87c5fe7513b3f
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-05-09 14:47:55 -0300

    Agrego al store los metodos que voy a utilizar sin su implmentacion.

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,8 +20,42 @@ const state = {
 const initStore = () => {
     console.log('Inicio Store...');
     console.log(state);
-}
+};
+
+const loadStore = () => {
+    throw new Error('Not implemented');
+};
+
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+};
+
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+};
+
+const deleteTodo = (todoId) => {
+    throw new Error('Not implemented');
+};
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+};
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+};
+
+const getFilter = () => {
+    throw new Error('Not implemented');
+};
 
 export default {
-    initStore
+    initStore,
+    loadStore,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
 };
