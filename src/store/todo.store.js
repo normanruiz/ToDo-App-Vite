192c6b28e40fff8ed97e1a728a840604e1c3455d
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-05-09 17:25:56 -0300

    Agrego la implementacion de los metodos de store.

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -11,7 +11,11 @@ const state = {
 
     todos: [
         new Todo('Emepzar el curso de VueJS.'),
-        new Todo('Continuar con el curso de Python.')
+        new Todo('Continuar con el curso de Python.'),
+        new Todo('Continuar con este curso.'),
+        new Todo('Terminar de leer el libro de Ruby Onm Rails.'),
+        new Todo('Continuar con el libro de fxRuby.'),
+        new Todo('Emepzar con el libro de Python.')
     ],
     filter: Filters.All
 
@@ -26,36 +30,57 @@ const loadStore = () => {
     throw new Error('Not implemented');
 };
 
+const getTodos = (filter = Filters.All) => {
+    switch( filter ){
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done );
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done );
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+    }
+};
+
 const addTodo = (description) => {
-    throw new Error('Not implemented');
+    if (!description) throw new Error('description is required');
+    state.todos.push(new Todo(description));
 };
 
 const toggleTodo = (todoId) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.map( todo => {
+        if (todo.id === todoId){
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
 };
 
 const deleteTodo = (todoId) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
 };
 
 const deleteCompleted = () => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter(todo => todo.done);
 };
 
 const setFilter = (newFilter = Filters.All) => {
-    throw new Error('Not implemented');
+    state.filter = newFilter;
 };
 
 const getFilter = () => {
-    throw new Error('Not implemented');
+    return state.filter;
 };
 
 export default {
     initStore,
     loadStore,
+    getTodos,
     addTodo,
     toggleTodo,
     deleteTodo,
     deleteCompleted,
     setFilter,
+    getFilter
 };
